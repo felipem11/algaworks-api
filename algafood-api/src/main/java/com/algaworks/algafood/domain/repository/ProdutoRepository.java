5e6d8e2def8415da94d3e15b001b069623667137
Felipe Martins <felipem11@outlook.com>
2020-12-09 13:24:11 +0100

    13.4. Implementando pesquisas simples na API

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/ProdutoRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/ProdutoRepository.java
@@ -1,14 +1,13 @@
 package com.algaworks.algafood.domain.repository;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.algaworks.algafood.domain.model.Produto;
-import com.algaworks.algafood.domain.model.Restaurante;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * 5.5. Desafio: refatorando todos os repositórios para usar SDJ<p>
@@ -18,7 +17,7 @@ import com.algaworks.algafood.domain.model.Restaurante;
  * 5.11. Implementando um repositório SDJ customizado<p>
  * 5.20. Estendendo o JpaRepository para customizar o repositório base<p>
  * 6.14. Resolvendo o Problema do N+1 com fetch join na JPQL<p>
- * @see  https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation
+ * @see  "https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation"
  * @author  Felipe Martins
  * @version 1.0
  * @since   2020-04-15 
@@ -27,13 +26,16 @@ import com.algaworks.algafood.domain.model.Restaurante;
 @Repository
 public interface ProdutoRepository extends CustomJpaRepository<Produto, Long>{
 
-	List<Produto> findByRestaurante(Restaurante restaurante);
-	
 	@Query("from Produto where restaurante.id = :restaurante and id = :produto")
-    Optional<Produto> findById(@Param("restaurante") Long restauranteId, 
-            @Param("produto") Long produtoId);
-	
-	List<Produto> findByIdAndRestaurante(Long produtoId, Long restauranteId);
+	Optional<Produto> findById(@Param("restaurante") Long restauranteId,
+							   @Param("produto") Long produtoId);
+
+	List<Produto> findTodosByRestaurante(Restaurante restaurante);
+
+	@Query("from Produto p where p.ativo = true and p.restaurante = :restaurante")
+	List<Produto> findAtivosByRestaurante(Restaurante restaurante);
+
+
 
 	
 	
