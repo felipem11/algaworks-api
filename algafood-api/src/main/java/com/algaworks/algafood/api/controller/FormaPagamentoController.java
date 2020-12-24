4302dc9a466534a5f3b35776617d636c166bf721
Felipe Martins <felipem11@outlook.com>
2020-12-24 10:46:34 +0100

    18.6. Descrevendo informações da API na documentação

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -80,7 +80,20 @@ public class FormaPagamentoController {
 	}
 
 	@GetMapping("/{formaPagamentoId}")
-	public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId) {
+	public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId, ServletWebRequest request) {
+		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+		String eTag = "0";
+		OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getDataAtualizacaoById(formaPagamentoId);
+
+		if (dataUltimaAtualizacao != null){
+			eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+		}
+
+		if(request.checkNotModified(eTag)){
+			return null;
+		}
+
 		FormaPagamento formaPagamento = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
 
 		FormaPagamentoModel formaPagamentoModel =  formaPagamentoModelAssembler.toModel(formaPagamento);
---
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -0,0 +1,62 @@
+package com.algaworks.algafood.core.openapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Server;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+/**
+ * 18.5. Selecionando os endpoints da API para gerar a documentação<br>
+ * 18.6. Descrevendo informações da API na documentação<br>
+ * @author  Felipe Martins
+ * @version 1.0
+ * @since   2020-04-15
+ */
+
+@Configuration
+@EnableOpenApi
+public class SpringFoxConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Docket api() {
+        Server serverLocal = new Server("local", "http://localhost:8080", "for local usages", Collections.emptyList(), Collections.emptyList());
+        Server testServer = new Server("test", "https://example.org", "for testing", Collections.emptyList(), Collections.emptyList());
+        return new Docket(DocumentationType.OAS_30)
+                .servers(serverLocal, testServer)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Felipe's AlgaFood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1.0.0")
+                .contact(new Contact("Felipe", "https://www.linkedin.com/in/felipe-martins01", "felipem11@outlook.com"))
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
